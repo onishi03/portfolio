65ad29a1be3993ff923ce9de7168cf7dd6f4bdb1
Rodrigo Onishi <rodrigo@teste.com>
2024-05-03 14:43:11 -0300

    aula de portfolio

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/onishi03.png" />
+    <Titulo fontSize={20}>Rodrigo Onishi</Titulo>
+  </aside>
+)
+
+export default Sidebar
