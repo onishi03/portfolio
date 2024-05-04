cfb6e1b7acb9ffd09dfd0069c6cb6c7474749cca
Rodrigo Onishi <rodrigo@teste.com>
2024-05-03 22:47:00 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
-
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/onishi03.png" />
-    <Titulo fontSize={20}>Rodrigo Onishi</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rodrigo Onishi</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Onishi03
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Troca tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
